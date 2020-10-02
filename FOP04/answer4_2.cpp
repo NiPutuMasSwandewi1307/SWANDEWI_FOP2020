fee50d1099b021b1d9f6da974b8cae65946dc8d2
NiPutuMasSwandewi1307 <niputumasswandewi1307@gmail.com>
2020-10-02 15:20:02 +0700

    FOP04

--- FOP04/answer4_2.cpp
+++ FOP04/answer4_2.cpp
@@ -14,57 +14,57 @@ int main (){
     switch(month)
     {
         case 1 :
-            cout<<"\n January"<<year<<", it has 31 days";
+            cout<<"\n January "<<year<<", it has 31 days";
             break;
 
         case 2 :
         //Condition if this is leap year or not
             if (((year%4==0) && (year%100 !=0)) || (year%400==0)){
-                cout<<"\nFebruary"<<year<<", it has 29 days";
+                cout<<"\nFebruary "<<year<<", it has 29 days";
             }
             else{
-                cout<<"\nFebruary"<<year<<", it has 28 days";
+                cout<<"\nFebruary "<<year<<", it has 28 days";
             }
             break;
         
         case 3:
-            cout<<"\n March"<<year<<", it has 31 days";
+            cout<<"\n March "<<year<<", it has 31 days";
             break;
 
         case 4:
-            cout<<"\n April"<<year<<", it has 30 days";
+            cout<<"\n April "<<year<<", it has 30 days";
             break;
         
         case 5:
-            cout<<"\n May"<<year<<", it has 31 days";
+            cout<<"\n May "<<year<<", it has 31 days";
             break;
 
         case 6:
-            cout<<"\n June"<<year<<", it has 30 days";
+            cout<<"\n June "<<year<<", it has 30 days";
             break;
         
         case 7:
-            cout<<"\n July"<<year<<", it has 31 days";
+            cout<<"\n July "<<year<<", it has 31 days";
             break;
 
         case 8:
-            cout<<"\n August"<<year<<", it has 31 days";
+            cout<<"\n August "<<year<<", it has 31 days";
             break;
 
         case 9:
-            cout<<"\n September"<<year<<", it has 30 days";
+            cout<<"\n September "<<year<<", it has 30 days";
             break;
         
         case 10:
-            cout<<"\n October"<<year<<", it has 31 days";
+            cout<<"\n October "<<year<<", it has 31 days";
             break;
         
         case 11:
-            cout<<"\n November"<<year<<", it has 30 days";
+            cout<<"\n November "<<year<<", it has 30 days";
             break;
         
         case 12:
-            cout<<"\n December"<<year<<", it has 31 days";
+            cout<<"\n December "<<year<<", it has 31 days";
             break;
     }
     return 0;
