58677d5a2e94c970c9d301b9d506784854b53bed
NiPutuMasSwandewi1307 <niputumasswandewi1307@gmail.com>
2020-10-09 12:00:06 +0700

    Yuk semangat

--- FOP05/answer5_2.cpp
+++ FOP05/answer5_2.cpp
@@ -5,25 +5,21 @@ using namespace std;
 
 int main ()
 {
-  //Declare num to floar with random number
-  srand (time(0));
-  double num1 = rand() % 100;
-  for (double num1=0; num1 <10;num1++){
-  }
-  double num2 = rand() % 100;
-  for (double num2=0; num1 <10;num2++){
-  }
   int answersystem, answeruser;
+  int question = 1;
+  int score = 0;
   char op;
-  
-    
-  //Input the number
-  cout<<"Angka pertama : "<<num1<<endl;
-  cout<<"Angka kedua : "<<num2<<endl;
+
+  //Declare num to floar with random number
+    while (question<=10){
+      double num1 = rand() % 100;  
+       cout<<"Angka pertama : "<<num1<<endl;
+      double num2 = rand() % 100;
+        cout<<"Angka kedua : "<<num2<<endl;
 
   //The operation
-  cout<<"\n Select the operation that you need (+,-,*,/) = ";
-  cin>>op;
+    cout<<"\n Select the operation that you need (+,-,*,/) = ";
+    cin>>op;
 
   //Switch statement begins
     switch (op) {
@@ -66,7 +62,7 @@ int main ()
       else {
         cout<<"The answer is = ";
         cin>>answeruser;
-        answersystem=num1+num2;
+        answersystem=num1/num2;
         if (answeruser!=answersystem){
           cout<<"Your answer is wrong. The correct answer is = "<<answersystem<<endl;
         }
@@ -77,7 +73,18 @@ int main ()
       default:
         cout<<"\nError! The operator is wrong";
         break;
+    
+    } 
+    if (answeruser==answersystem){
+      score++;
+        cout<<"Your answer is Correct\n";
     }
+
+    cout<<"THE NEXT QUESTION\n";
+    question++;
+    } 
+    
+    cout<<"Your final score is "<<score<<" out of 10\n";
     return 0;
 
 }
