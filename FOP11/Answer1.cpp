b0e38a575b6d03489927fec98e5bfc40890bdcf1
NiPutuMasSwandewi1307 <niputumasswandewi1307@gmail.com>
2020-11-27 14:59:45 +0700

    FOP11

---
+++ FOP11/Answer1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#inclue <array>
+using namespace std;
+
+int main()
+{
+    //Mendeklarasi angka-angka dalam kelompok
+    int myList[5] = {4, 7, 12, 5, 2};
+    
+    cout<<myList[1]<<endl;
+}
